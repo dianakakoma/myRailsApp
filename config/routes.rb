66c70640360030e9ee7305e44d0480d8c49b35b8
dianakakoma <dikakoma@gmail.com>
2020-06-21 11:47:10 -0500

    init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root "welcome#index"
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # namespace :api do
+  #   get "/photos" => "photos#index"
+  # end
+end
